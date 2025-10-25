33ce489d8ad3955486e074a73efc7b8e40ecb66f
Sophie Pope <sophiepope393@gmail.com>
2025-10-25 16:17:33 +0700

    Updated the database to have the correct columns and datatypes

--- time_tracker_DB.sql
+++ time_tracker_DB.sql
@@ -2,4 +2,25 @@ CREATE DATABASE time_tracker_db;
 
 USE time_tracker_db;
 
+CREATE TABLE time_tracked(
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    app_name VARCHAR(100) NOT NULL,
+    info VARCHAR(100),
+    start_time TIME,
+    end_time TIME,
+    duration_seconds FLOAT(2)
+);
 
+ALTER TABLE time_tracked
+ADD start_date DATETIME,
+ADD end_date DATETIME;
+
+ALTER TABLE time_tracked
+RENAME COLUMN start_date to start,
+RENAME COLUMN end_date to end,
+DROP COLUMN start_time,
+DROP COLUMN end_time;
+
+SHOW TABLES;
+
+SELECT * FROM time_tracked;
